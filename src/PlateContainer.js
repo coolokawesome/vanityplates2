c80645dcb822c1b3b723a0248a06f043f3ac9aab
rickyjoe11 <syrus.samson99@gmail.com>
2023-05-25 22:40:54 -0400

    added ability to save the custom plate as an image

--- src/PlateContainer.js
+++ src/PlateContainer.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { RecoilState, useRecoilState, useRecoilValue } from 'recoil'
 import { plateImage, currentPlateImage, currentPlateColor, plateText } from './atoms.js'
 import './styles/PlateContainer.css'
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 
 function PlateContainer() {
@@ -36,10 +38,27 @@ switch(currentColor) {
         return 'plate-text text-green'
     }
 }
+//generate the image for download
+const generatePDF = () => {
+    const plateContainer = document.getElementById('plateContainer');
+  
+    // Create a canvas element from the target div
+    html2canvas(plateContainer).then((canvas) => {
+        // Convert the canvas to a data URL representing the PNG image
+        const imgData = canvas.toDataURL('image/png');
+        
+        // Create a temporary link element to trigger the download
+        const link = document.createElement('a');
+        link.href = imgData;
+        link.download = 'plate.png';
+        link.click();
+    });
+  };
+  
 
 return (
 <div className='col-12 col-lg-6'>
-    <div className='plate-container m-3 p-2'>
+    <div id='plateContainer' className='plate-container m-3 p-2'>
         <div>
         <img className='plate-img img img-fluid' id='plateImg' src={currentPlate}></img>
         {/* {run the text change function on rerender} */}
@@ -48,7 +67,7 @@ return (
     </div>
     <div className='download-row m-3'>
         <div className='d-flex justify-content-center align-items-end'>
-            <a className='download-img-button px-3 py-2'>
+            <a className='download-img-button px-3 py-2' onClick={generatePDF}>
                 <i class="fa-solid fa-download px-2"></i>
                 Download Image
             </a>
